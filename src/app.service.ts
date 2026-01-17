89923d0b878b3e12394b02f2c4cddf14a8c0c6f9
odiloviccc <odiloviccc@gmail.com>
2026-01-17 15:48:23 +0500

    test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! AutoTest API is running!';
+    return 'Autotest api';
   }
 }
